ecd3d57830dec288b7d0fa0dc19cda518b2bfd10
Virendra Kumar Patel <virendra.patel@news.co.uk>
2021-10-14 22:18:07 +0530

    removed unused

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Fragment } from "react";
+import ReactPortal from "./components/ReactPortal";
+import Form from "./components/Form";
+
+function App() {
+  return <Fragment></Fragment>;
+}
+
+export default App;
